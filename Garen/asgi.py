0a138e964d1bc2d9fd427de569e3c1ced5142d66
garenliu-dev <394080797@qq.com>
2020-03-29 22:24:50 +0800

    init

---
+++ Garen/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Garen project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Garen.settings')
+
+application = get_asgi_application()
